e745042ff23d6b2d5ef5eae7257896ba70bb47d3
Dharmendra Kumar Awasthi <dharmendrakumar.awasthi@aristocrat.com>
2021-10-21 21:48:47 +0530

    Init project

---
+++ src/main/java/com/techendear/api/DbdumpApplication.java
@@ -0,0 +1,13 @@
+package com.techendear.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbdumpApplication.class, args);
+	}
+
+}
