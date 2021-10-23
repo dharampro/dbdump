790f74acfba462549ee9a41148168ea6bbf13193
Dharmendra Kumar Awasthi <dharmendrakumar.awasthi@aristocrat.com>
2021-10-23 17:14:04 +0530

    Added CRUD controller

--- src/main/java/com/techendear/api/entity/User.java
+++ src/main/java/com/techendear/api/entity/User.java
@@ -15,7 +15,7 @@ public class User {
 
     @Id
     private String id;
-    private String userName;
+    private String firstName;
     private String lasteName;
     private String email;
     private String contactNumber;
